9731170554d766c8cc68121c1cdda6fecaead394
Olybear9 <olybear9@gmail.com>
2020-04-28 17:41:05 -0600

    Update: Change this to be simpler

--- start.cmd
+++ start.cmd
@@ -3,15 +3,13 @@
 if exist node_modules (
     goto run
 ) else (
-    goto install
+    echo Installing needed dependencies, give this a moment.
+    npm i > nul
+    echo Done, attempting to start.
+    goto run
 )
-
-:install
-echo Installing needed dependencies, give this a moment.
-npm i > nul
-echo Done, attempting to start.
-goto run
 
 :run
 echo Starting...
 npm run "start"
+exit
