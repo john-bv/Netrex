8f92a53573a3cc1e10b77c8386e903f9748beaf2
CadetMercury615 <60723938+CadetMercury615@users.noreply.github.com>
2020-05-09 22:51:49 -0400

    Create ServerInfo.ts

---
+++ src/command/default/ServerInfo.ts
@@ -0,0 +1,39 @@
+/***
+ *      _   _      _
+ *     | \ | |    | |
+ *     |  \| | ___| |_ _ __ _____  __
+ *     | . ` |/ _ \ __| '__/ _ \ \/ /
+ *     | |\  |  __/ |_| | |  __/>  <
+ *     |_| \_|\___|\__|_|  \___/_/\_\
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ */
+import Command from '@/command/Command';
+import CommandSender from '@/command/senders/CommandSender';
+import CommandManager from '../CommandManager';
+import Server from '@/server/Server';
+
+class ServerInfoCommand extends Command {
+    public prepare(): void {
+        this.permissions = [];
+        this.permissions.push('netrex.command.stop');
+        this.operators = true;
+        this.restricted = true;
+    }
+
+    public onRun(sender: CommandSender, label: string, args: Array<string>): void {
+        sender.sendMessage(`Server Name: ${this.getServer().ServerName}`)
+        sender.sendMessage(`Server Address: ${this.getServer().Server_IP}`)
+        sender.sendMessage(`Server Port: ${this.getServer().Port}`)
+        sender.sendMessage(`Server MOTD: ${this.getServer().MOTD}`)
+        sender.sendMessage(`Max Players: ${this.getServer().MaxPlayers}`) // <== this is undefined for some reason
+
+
+        
+    }
+}
+
+export default ServerInfoCommand;
